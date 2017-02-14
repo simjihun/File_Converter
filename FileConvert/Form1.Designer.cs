98830c3f419fc79256ddae0156c4e4b9fc60f8a2
jihun Sim <simering1989@gmail.com>
2017-02-14 18:01:50 +0900

    2.14

--- FileConvert/Form1.Designer.cs
+++ FileConvert/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// 필수 디자이너 변수입니다.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 사용 중인 모든 리소스를 정리합니다.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,225 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 디자이너에서 생성한 코드
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
-        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.메뉴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.폴더열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.버전정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSavePath = new System.Windows.Forms.TextBox();
+            this.btnSavePath = new System.Windows.Forms.Button();
+            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnConvert2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(31, 77);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(297, 21);
+            this.txtPath.TabIndex = 0;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(339, 61);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(98, 37);
+            this.btnPath.TabIndex = 1;
+            this.btnPath.Text = "읽을파일선택";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.메뉴ToolStripMenuItem,
+            this.도움말ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(574, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 메뉴ToolStripMenuItem
+            // 
+            this.메뉴ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.폴더열기ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.메뉴ToolStripMenuItem.Name = "메뉴ToolStripMenuItem";
+            this.메뉴ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.메뉴ToolStripMenuItem.Text = "메뉴";
+            // 
+            // 폴더열기ToolStripMenuItem
+            // 
+            this.폴더열기ToolStripMenuItem.Name = "폴더열기ToolStripMenuItem";
+            this.폴더열기ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.폴더열기ToolStripMenuItem.Text = "자동변환폴더열기";
+            this.폴더열기ToolStripMenuItem.Click += new System.EventHandler(this.폴더열기ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // 도움말ToolStripMenuItem
+            // 
+            this.도움말ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.버전정보ToolStripMenuItem});
+            this.도움말ToolStripMenuItem.Name = "도움말ToolStripMenuItem";
+            this.도움말ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.도움말ToolStripMenuItem.Text = "도움말";
+            // 
+            // 버전정보ToolStripMenuItem
+            // 
+            this.버전정보ToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("버전정보ToolStripMenuItem.Image")));
+            this.버전정보ToolStripMenuItem.Name = "버전정보ToolStripMenuItem";
+            this.버전정보ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.버전정보ToolStripMenuItem.Text = "버전정보";
+            this.버전정보ToolStripMenuItem.Click += new System.EventHandler(this.버전정보ToolStripMenuItem_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(155, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "주의 : 중복 변환하면 안됨";
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Location = new System.Drawing.Point(31, 160);
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.ReadOnly = true;
+            this.txtSavePath.Size = new System.Drawing.Size(297, 21);
+            this.txtSavePath.TabIndex = 6;
+            // 
+            // btnSavePath
+            // 
+            this.btnSavePath.Location = new System.Drawing.Point(339, 149);
+            this.btnSavePath.Name = "btnSavePath";
+            this.btnSavePath.Size = new System.Drawing.Size(98, 34);
+            this.btnSavePath.TabIndex = 7;
+            this.btnSavePath.Text = "경로직접지정";
+            this.btnSavePath.UseVisualStyleBackColor = true;
+            this.btnSavePath.Click += new System.EventHandler(this.btnSavePath_Click);
+            // 
+            // sfdFile
+            // 
+            this.sfdFile.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(457, 149);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(98, 34);
+            this.btnConvert.TabIndex = 8;
+            this.btnConvert.Text = "직접변환";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnConvert2
+            // 
+            this.btnConvert2.Location = new System.Drawing.Point(457, 61);
+            this.btnConvert2.Name = "btnConvert2";
+            this.btnConvert2.Size = new System.Drawing.Size(98, 37);
+            this.btnConvert2.TabIndex = 9;
+            this.btnConvert2.Text = "자동변환";
+            this.btnConvert2.UseVisualStyleBackColor = true;
+            this.btnConvert2.Click += new System.EventHandler(this.btnConvert2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "*- 자동변환";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "*- 저장경로 지정";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(574, 203);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConvert2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnSavePath);
+            this.Controls.Add(this.txtSavePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "File Converter _ Tide";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 메뉴ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 버전정보ToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSavePath;
+        private System.Windows.Forms.Button btnSavePath;
+        private System.Windows.Forms.SaveFileDialog sfdFile;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnConvert2;
+        private System.Windows.Forms.ToolStripMenuItem 폴더열기ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
-
--- FileConvert/Form1.cs
+++ FileConvert/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,143 @@ namespace FileConvert
 {
     public partial class Form1 : Form
     {
+        String txtSave = "itide";
+        String txt = "";
+        String saveC = @"C:\TideConvert";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if (this.ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPath.Text = this.ofdFile.FileName; // ofdFile 경로를 Text속성에 저장
+            }
+        }
+        
+        
+        private bool txtCheck()
+        {
+            if (this.txtPath.Text == "")
+                return false;
+            else
+                return true;
+        }
+
+        private bool txtSaveCheck()
+        {
+            if (this.txtSavePath.Text == "")
+                return false;
+            else
+                return true;
+        }
+
+        private void btnSavePath_Click(object sender, EventArgs e)
+        {
+            if (this.sfdFile.ShowDialog() == DialogResult.OK)
+            {
+                this.txtSavePath.Text = this.sfdFile.FileName; // 저장경로지정
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+                if (txtCheck() == false)
+                    return;
+                if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(this.txtPath.Text, Encoding.Default))
+                        {
+                            txt = sr.ReadToEnd();
+                            sr.Close();
+                        }
+                        File.AppendAllText(txtSavePath.Text, txtSave + txt, Encoding.Default);
+                        MessageBox.Show("파일이 정상적으로 변환되었습니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }catch(IOException){
+                        MessageBox.Show("저장경로를 지정해주세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }else{
+                    MessageBox.Show("변환할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            //File.WriteAllText(txtSavePath.Text, txtSave + txtView.Text);
+        }
+        
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConvert2_Click(object sender, EventArgs e)
+        {
+            if (txtCheck() == false)
+                return;
+            if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(this.txtPath.Text, Encoding.Default))
+                    {
+                        txt = sr.ReadToEnd();              
+                    }
+
+                    int position = txtPath.Text.LastIndexOf("\\");
+                    string fileName = txtPath.Text.Substring(position + 1); //파일명+확장자 추출
+                    char[] delimeters = {'.'};
+                    string[] dirSplit = fileName.Split(delimeters);
+                    //string saveAuto = "tide_" + fileName;      //앞에 tide를 붙일때
+                    string saveAuto = dirSplit[0] +"_tide" +"." + dirSplit[1];  //스플릿으로 나눔!
+                    string newDir = saveC + @"\" + saveAuto;
+                    string con = txtSave + txt;
+
+
+                    //File.AppendAllText(saveC + saveAuto, txtSave + txt, Encoding.Default);
+                    //MessageBox.Show("값을 알아보자 : " + saveC + saveAuto, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (!Directory.Exists(saveC))
+                    {
+                        Directory.CreateDirectory(saveC);
+                    }
+                    if (File.Exists(newDir))
+                    {
+                        
+                    }else
+                    {
+                        File.AppendAllText(newDir, con, Encoding.Default);
+                    }
+                    
+                    //Byte[] info = new UTF8Encoding(true).GetBytes(txt.Insert(0, txtSave));
+                    //FileStream fs = File.Open(saveC+saveAuto, FileMode.Append);
+                    //fs.Write(info, 0, info.Length);
+                    //fs.Close();
+                    //MessageBox.Show("값을 알아보자 : " + info.ToString(), "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("파일이 [" + newDir + "] 경로로 변환되었습니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("에러발생", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("변환할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void 버전정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 폴더열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe",saveC);            
         }
     }
 }
