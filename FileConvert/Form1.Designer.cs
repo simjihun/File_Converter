580c80070b87e08e2ea679f2720901186107693c
jihun Sim <simering1989@gmail.com>
2017-02-21 16:13:52 +0900

    BIN파일변환+CRC체크기능구현

--- FileConvert/Form1.Designer.cs
+++ FileConvert/Form1.Designer.cs
@@ -38,14 +38,10 @@
             this.도움말ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.버전정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ofdFile = new System.Windows.Forms.OpenFileDialog();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtSavePath = new System.Windows.Forms.TextBox();
-            this.btnSavePath = new System.Windows.Forms.Button();
             this.sfdFile = new System.Windows.Forms.SaveFileDialog();
-            this.btnConvert = new System.Windows.Forms.Button();
             this.btnConvert2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,12 +50,12 @@
             this.txtPath.Location = new System.Drawing.Point(31, 77);
             this.txtPath.Name = "txtPath";
             this.txtPath.ReadOnly = true;
-            this.txtPath.Size = new System.Drawing.Size(297, 21);
+            this.txtPath.Size = new System.Drawing.Size(270, 21);
             this.txtPath.TabIndex = 0;
             // 
             // btnPath
             // 
-            this.btnPath.Location = new System.Drawing.Point(339, 61);
+            this.btnPath.Location = new System.Drawing.Point(319, 61);
             this.btnPath.Name = "btnPath";
             this.btnPath.Size = new System.Drawing.Size(98, 37);
             this.btnPath.TabIndex = 1;
@@ -74,7 +70,7 @@
             this.도움말ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(574, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(635, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -119,93 +115,51 @@
             // 
             // ofdFile
             // 
-            this.ofdFile.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("나눔고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(155, 112);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "주의 : 중복 변환하면 안됨";
-            // 
-            // txtSavePath
-            // 
-            this.txtSavePath.Location = new System.Drawing.Point(31, 160);
-            this.txtSavePath.Name = "txtSavePath";
-            this.txtSavePath.ReadOnly = true;
-            this.txtSavePath.Size = new System.Drawing.Size(297, 21);
-            this.txtSavePath.TabIndex = 6;
-            // 
-            // btnSavePath
-            // 
-            this.btnSavePath.Location = new System.Drawing.Point(339, 149);
-            this.btnSavePath.Name = "btnSavePath";
-            this.btnSavePath.Size = new System.Drawing.Size(98, 34);
-            this.btnSavePath.TabIndex = 7;
-            this.btnSavePath.Text = "경로직접지정";
-            this.btnSavePath.UseVisualStyleBackColor = true;
-            this.btnSavePath.Click += new System.EventHandler(this.btnSavePath_Click);
+            this.ofdFile.Filter = "Binary 파일(*.bin)|*.bin|모든 파일(*.*)|*.*|텍스트 파일 (*.txt) |*.txt";
             // 
             // sfdFile
             // 
-            this.sfdFile.Filter = "텍스트 파일 (*.txt) |*.txt|모든 파일(*.*)|*.*";
-            // 
-            // btnConvert
-            // 
-            this.btnConvert.Location = new System.Drawing.Point(457, 149);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(98, 34);
-            this.btnConvert.TabIndex = 8;
-            this.btnConvert.Text = "직접변환";
-            this.btnConvert.UseVisualStyleBackColor = true;
-            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            this.sfdFile.Filter = "모든 파일(*.bin)|*.bin|모든 파일(*.*)|*.*|텍스트 파일 (*.txt) |*.txt";
             // 
             // btnConvert2
             // 
-            this.btnConvert2.Location = new System.Drawing.Point(457, 61);
+            this.btnConvert2.Location = new System.Drawing.Point(527, 61);
             this.btnConvert2.Name = "btnConvert2";
             this.btnConvert2.Size = new System.Drawing.Size(98, 37);
             this.btnConvert2.TabIndex = 9;
-            this.btnConvert2.Text = "자동변환";
+            this.btnConvert2.Text = "CRC 체크";
             this.btnConvert2.UseVisualStyleBackColor = true;
             this.btnConvert2.Click += new System.EventHandler(this.btnConvert2_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Font = new System.Drawing.Font("나눔고딕 ExtraBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(316, 34);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 19);
+            this.label2.Size = new System.Drawing.Size(183, 15);
             this.label2.TabIndex = 10;
-            this.label2.Text = "*- 자동변환";
+            this.label2.Text = "*- 저장경로 : C:\\TideConvert";
             // 
-            // label3
+            // btnTest
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(12, 121);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 19);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "*- 저장경로 지정";
+            this.btnTest.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnTest.Location = new System.Drawing.Point(423, 61);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(98, 37);
+            this.btnTest.TabIndex = 11;
+            this.btnTest.Text = "BIN 파일 변환";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 203);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(635, 121);
+            this.Controls.Add(this.btnTest);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnConvert2);
-            this.Controls.Add(this.btnConvert);
-            this.Controls.Add(this.btnSavePath);
-            this.Controls.Add(this.txtSavePath);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnPath);
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.menuStrip1);
@@ -231,14 +185,10 @@
         private System.Windows.Forms.ToolStripMenuItem 도움말ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 버전정보ToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog ofdFile;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtSavePath;
-        private System.Windows.Forms.Button btnSavePath;
         private System.Windows.Forms.SaveFileDialog sfdFile;
-        private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Button btnConvert2;
         private System.Windows.Forms.ToolStripMenuItem 폴더열기ToolStripMenuItem;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTest;
     }
 }
--- FileConvert/Form1.cs
+++ FileConvert/Form1.cs
@@ -13,9 +13,10 @@ namespace FileConvert
 {
     public partial class Form1 : Form
     {
-        String txtSave = "itide";
+        String txtSave = "itide1.0 ";
         String txt = "";
         String saveC = @"C:\TideConvert";
+
 
         public Form1()
         {
@@ -28,8 +29,7 @@ namespace FileConvert
             {
                 this.txtPath.Text = this.ofdFile.FileName; // ofdFile 경로를 Text속성에 저장
             }
-        }
-        
+        }        
         
         private bool txtCheck()
         {
@@ -38,113 +38,12 @@ namespace FileConvert
             else
                 return true;
         }
-
-        private bool txtSaveCheck()
-        {
-            if (this.txtSavePath.Text == "")
-                return false;
-            else
-                return true;
-        }
-
-        private void btnSavePath_Click(object sender, EventArgs e)
-        {
-            if (this.sfdFile.ShowDialog() == DialogResult.OK)
-            {
-                this.txtSavePath.Text = this.sfdFile.FileName; // 저장경로지정
-            }
-        }
-
-        private void btnConvert_Click(object sender, EventArgs e)
-        {
-                if (txtCheck() == false)
-                    return;
-                if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
-                {
-                    try
-                    {
-                        using (StreamReader sr = new StreamReader(this.txtPath.Text, Encoding.Default))
-                        {
-                            txt = sr.ReadToEnd();
-                            sr.Close();
-                        }
-                        File.AppendAllText(txtSavePath.Text, txtSave + txt, Encoding.Default);
-                        MessageBox.Show("파일이 정상적으로 변환되었습니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }catch(IOException){
-                        MessageBox.Show("저장경로를 지정해주세요", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                }else{
-                    MessageBox.Show("변환할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            //File.WriteAllText(txtSavePath.Text, txtSave + txtView.Text);
-        }
         
-
         private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        private void btnConvert2_Click(object sender, EventArgs e)
-        {
-            if (txtCheck() == false)
-                return;
-            if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
-            {
-                try
-                {
-                    using (StreamReader sr = new StreamReader(this.txtPath.Text, Encoding.Default))
-                    {
-                        txt = sr.ReadToEnd();              
-                    }
-
-                    int position = txtPath.Text.LastIndexOf("\\");
-                    string fileName = txtPath.Text.Substring(position + 1); //파일명+확장자 추출
-                    char[] delimeters = {'.'};
-                    string[] dirSplit = fileName.Split(delimeters);
-                    //string saveAuto = "tide_" + fileName;      //앞에 tide를 붙일때
-                    string saveAuto = dirSplit[0] +"_tide" +"." + dirSplit[1];  //스플릿으로 나눔!
-                    string newDir = saveC + @"\" + saveAuto;
-                    string con = txtSave + txt;
-
-
-                    //File.AppendAllText(saveC + saveAuto, txtSave + txt, Encoding.Default);
-                    //MessageBox.Show("값을 알아보자 : " + saveC + saveAuto, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    if (!Directory.Exists(saveC))
-                    {
-                        Directory.CreateDirectory(saveC);
-                    }
-                    if (File.Exists(newDir))
-                    {
-                        MessageBox.Show("변환한 파일이 이미 존재합니다. 삭제하고 다시 변환해주세요.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    else
-                    {
-                        File.AppendAllText(newDir, con, Encoding.Default);
-                        MessageBox.Show("파일이 [" + newDir + "] 경로로 변환되었습니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }                    
-                    //Byte[] info = new UTF8Encoding(true).GetBytes(txt.Insert(0, txtSave));
-                    //FileStream fs = File.Open(saveC+saveAuto, FileMode.Append);
-                    //fs.Write(info, 0, info.Length);
-                    //fs.Close();             
-                }
-                catch (IOException ioe)
-                {
-                    MessageBox.Show("에러발생 : " + ioe.Message , "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-                catch(AccessViolationException ave)
-                {
-                    MessageBox.Show("에러발생 : " + ave.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-            }
-            else
-            {
-                MessageBox.Show("변환할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
 
         private void 버전정보ToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -156,5 +55,199 @@ namespace FileConvert
         {
             System.Diagnostics.Process.Start("explorer.exe",saveC);            
         }
+
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            
+            if (txtCheck() == false)
+                return;
+            if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
+            {
+                try
+                {
+                    //txt형식 default
+                    using (StreamReader sr = new StreamReader(this.txtPath.Text, Encoding.Default))
+                    {
+                        txt = sr.ReadToEnd();
+                        sr.Close();
+                    }
+                    
+                    int position = txtPath.Text.LastIndexOf("\\");
+                    string fileName = txtPath.Text.Substring(position + 1); 
+                    char[] delimeters = { '.' };
+                    string[] dirSplit = fileName.Split(delimeters);
+                    string saveAuto = dirSplit[0] + "_itide" + "." + dirSplit[1];  
+                    string newDir = saveC + @"\" + saveAuto;
+                    string con = txtSave + txt; 
+
+                    //bin형식unicode
+                    byte[] dataTide = Encoding.Default.GetBytes("itide1.0");
+                    byte[] dataRead = File.ReadAllBytes(txtPath.Text);
+                    String strData = System.Text.Encoding.Unicode.GetString(dataRead);
+                    String strTide = System.Text.Encoding.Unicode.GetString(dataTide);
+                    String strTotal = strTide + strData;
+                    byte[] byteTotal = System.Text.Encoding.Unicode.GetBytes(strTotal);  //여기까지는 itide1.0 + 원데이터
+
+
+                    ushort crcCheck = CalcCRC(byteTotal); //CRC 코드(itide1.0+data)
+
+
+                    String crcCode = crcCheck.ToString("X");
+                    byte[] crcByte = Encoding.Default.GetBytes(crcCode);
+                    String crcCode1 = Encoding.Unicode.GetString(crcByte);
+                    String strFinish = strTotal + crcCode1;
+                    byte[] byteFinish = Encoding.Unicode.GetBytes(strFinish); // itide1.0+data+crc
+
+
+
+                    if (!Directory.Exists(saveC))
+                    {
+                        Directory.CreateDirectory(saveC);
+                    }
+                    if (File.Exists(newDir))
+                    {
+                        MessageBox.Show("변환한 파일이 이미 존재합니다. 삭제하고 다시 변환해주세요.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        if (dirSplit[1]=="bin")
+                        {
+                            File.WriteAllBytes(newDir, byteFinish);
+                        }else if(dirSplit[1] == "txt")
+                        {
+                            File.AppendAllText(newDir, con, Encoding.Default);
+                        }
+                        MessageBox.Show("파일이 [" + newDir + "] 경로로 변환되었습니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        //MessageBox.Show("CRC 코드는 [" + crcCode + "] 입니다.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);                
+                    }
+                            
+                }
+                catch (IOException ioe)
+                {
+                    MessageBox.Show("에러발생 : " + ioe.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                catch (AccessViolationException ave)
+                {
+                    MessageBox.Show("에러발생 : " + ave.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("변환할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static ushort CalcCRC(byte[] strPacket)
+        {
+            ushort[] CRC16_TABLE = { 0x0000, 0xCC01, 0xD801, 0x1400, 0xF001, 0x3C00, 0x2800, 0xE401, 0xA001, 0x6C00, 0x7800, 0xB401, 0x5000, 0x9C01, 0x8801, 0x4400 };
+            ushort usCRC = 0xFFFF;
+            ushort usTemp = 0;
+
+            foreach (char cCurrent in strPacket)
+            {
+                byte bytCurrent = Convert.ToByte(cCurrent);// lower 4 bits 
+                usTemp = CRC16_TABLE[usCRC & 0x000F];
+                usCRC = (ushort)((usCRC >> 4) & 0x0FFF);
+                usCRC = (ushort)(usCRC ^ usTemp ^ CRC16_TABLE[bytCurrent & 0x000F]); // Upper 4 Bits 
+                usTemp = CRC16_TABLE[usCRC & 0x000F];
+                usCRC = (ushort)((usCRC >> 4) & 0x0FFF);
+                usCRC = (ushort)(usCRC ^ usTemp ^ CRC16_TABLE[(bytCurrent >> 4) & 0x000F]);
+            }
+            return usCRC;
+        }
+
+        public static string GetStringByteCollection(string StringValue, int ByteCnt)
+        {
+            string lsTmpStringValue = string.Empty;
+            string lsReturnValue = string.Empty;
+            int liByteCnt = 0;
+            try
+            {
+                for (int liStartPoint = 0; liStartPoint < StringValue.Length; liStartPoint++)
+                {
+                    lsTmpStringValue = lsTmpStringValue + StringValue.Substring(liStartPoint, 1).Replace("'", "''");
+                    liByteCnt = Encoding.Default.GetByteCount(lsTmpStringValue);
+
+                    if (liByteCnt > ByteCnt)
+                    {
+                        lsReturnValue = lsTmpStringValue.Substring(0, lsTmpStringValue.Length - 1);
+                        break;
+                    }
+                    else
+                    {
+                        lsReturnValue = lsTmpStringValue.ToString();
+                    }
+                }
+                return lsReturnValue.ToString();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private void btnConvert2_Click(object sender, EventArgs e)
+        {
+            if (txtCheck() == false)
+                return;
+            if (File.Exists(this.txtPath.Text)) // 지정된 경로에 파일이 존재하는지 검사
+            {
+                try
+                {
+                    FileStream fs = File.OpenRead(txtPath.Text);
+                    BinaryReader br = new BinaryReader(fs);        
+                    
+                    //정상적인지 위변조되있는지 모르는 데이터를 불러와서 itide1.0+data부분을 crc 체크       
+                    //[검사CRC]   
+                    byte[] dataAllRead = File.ReadAllBytes(txtPath.Text);   //itide1.0+data+crc가포함된데이터
+                    int size = dataAllRead.Length-4;
+                    byte[] byteSub = br.ReadBytes(size);    // itide1.0+data만 포함된 데이터 (crc 제외)
+                    ushort crcCheck = CalcCRC(byteSub);
+                    string crcCode = crcCheck.ToString("X"); // 변조되었는지 byteSub을 다시한번 crc 체크함
+
+                    //itide로 변환된 데이터 하단에 마킹되있는 crc를 불러옴
+                    //[기존CRC]
+                    String strDataAllRead = Encoding.Unicode.GetString(dataAllRead);
+                    String strCRC = strDataAllRead.Substring(dataAllRead.Length/2-2,2);
+                    byte[] byteCRC = Encoding.Unicode.GetBytes(strCRC);
+                    string strCRCutf = Encoding.UTF8.GetString(byteCRC);
+
+
+                    if (strCRCutf == crcCode)
+                    {
+                        MessageBox.Show("CRC 일치!! 기존 CRC [ " + strCRCutf + " ] + 검사 CRC [ " + crcCode + " ] 정상적인 파일입니다.", "정상적인파일", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else if (strCRC != crcCode)
+                    {
+                        MessageBox.Show("CRC가 등록되어 있지 않거나, 파일이 변조되어 CRC가 일치하지 않습니다.", "불일치파일", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        MessageBox.Show("에러", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    fs.Close();
+                    br.Close();                    
+                                               
+                }
+                catch (IOException ioe)
+                {
+                    MessageBox.Show("에러발생 : " + ioe.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                catch (AccessViolationException ave)
+                {
+                    MessageBox.Show("에러발생 : " + ave.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("CRC 체크를 할 파일을 선택해주세요..", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
